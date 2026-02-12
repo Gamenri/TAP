fb3373c922b50619cd6f8ed08f089d1e30a63ae2
gamam <gamalielpatinom@gmail.com>
2026-02-12 10:00:00 -0600

    Clase 12/02
    Metodo metodo agregado con elementos en el frame de LogIn

--- TAP/U1/Empleados/Empleados.java
+++ TAP/U1/Empleados/Empleados.java
@@ -2,11 +2,13 @@ package Empleados;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 class FrameInicio extends JFrame{
     JFrame frameInicio = new JFrame();
     String usuario = "";
-    JButton botonEntrar = new JButton();
+    JButton botonEntrar = new JButton("INGRESAR");
     JLabel ETQclave = new JLabel("CAPTURAR CLAVE DE ACCESO");
     JTextField txtClave = new JTextField("");
     JLabel Titulo = new JLabel("SISTEMA");
@@ -19,10 +21,42 @@ class FrameInicio extends JFrame{
         frameInicio.setLocationRelativeTo(null);
         frameInicio.setLayout(null);
     }// consturctor
-}// Clase Frame
+
+    void metodoFrameInicio () {
+        frameInicio.add(Titulo);
+        frameInicio.add(ETQclave);
+        frameInicio.add(txtClave);
+        frameInicio.add(botonEntrar);
+
+        Titulo.setBounds(70,70,1500,80);
+        Titulo.setFont(new Font("Arial",Font.BOLD,40));
+        ETQclave.setBounds(200,160,1300,30);
+        ETQclave.setFont(new Font("Arial",Font.PLAIN,40));
+        ETQclave.setForeground(Color.BLUE);
+        txtClave.setBounds(200,250,300,80);
+        txtClave.setFont(new Font("Arial",Font.PLAIN,50));
+        txtClave.setForeground(Color.GREEN);
+        botonEntrar.setBounds(300,450,300,80);
+        botonEntrar.setFont(new Font("Arial",Font.PLAIN,30));
+        botonEntrar.setForeground(Color.red);
+        frameInicio.setVisible(true);
+        int Intentos = 0;
+        botonEntrar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                usuario=txtClave.getText();
+                if (usuario.contentEquals("123")) {
+                    JOptionPane.showMessageDialog(null,"CLAVE CORRECTA");
+                    Empleados objeto = new Empleados();
+                }
+            }
+        });
+    }// Metodo metodoFrameInicio
+}// Clase FrameInicio
 
 public class Empleados {
     public static void main(String[] args) {
-
+        FrameInicio login = new FrameInicio();
+        login.metodoFrameInicio();
     }// main
 }// Clase Empleados
